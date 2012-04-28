1d6b957ccf520620ac9f282c58ca76ff417d48dc
Jay Patel <jaypatel@jay-patels-macbook-pro.local>
2012-04-28 05:29:32 -0400

    django changes

--- wsgi/openshift/views.py
+++ wsgi/openshift/views.py
@@ -1,5 +1,20 @@
 import os
+import pymongo
 from django.shortcuts import render_to_response
+from django.utils import simplejson
+
+connection = pymongo.Connection('mongodb://admin:4A5buhckPFSw@127.6.89.1:27017/')
+#connection = pymongo.Connection('localhost', 27017)
+db = connection.six
 
 def home(request):
     return render_to_response('home/home.html')
+
+
+
+def getActors(request):
+    name = request.POST.get('name', False)
+
+    adb = db.actors.find_one({"name":name})
+
+    return HttpResponse(simplejson.dumps(adb), mimetype='application/javascript')
